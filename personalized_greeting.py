09ad1d4d1a419dfb75ab8733bd023d2fe4c518db
Cory Gunter <cory.j.gunter@gmail.com>
2024-06-07 13:16:49 -0400

    Moved exercises into exercise folder

---
+++ personalized_greeting.py
@@ -0,0 +1,12 @@
+# Personalized Greeting
+
+print("What's your name?")
+name = input()
+
+print(f'Good morning, {name}!')
+
+name = input("What's your name? ")
+print(f'Good morning, {name}!')
+
+name = input("What's your name?\n")
+print(f'Good morning, {name}!')
---
+++ sum_numbers.py
@@ -0,0 +1,19 @@
+# Sum Numbers
+
+number1 = input('Enter the first number: ')
+number2 = input('Enter the second number: ')
+number3 = input('Enter the third number: ')
+number4 = input('Enter the fourth number: ')
+
+
+sum = float(number1) + float(number2)
+total = float(number1) * float(number2) * float(number3) * float(number4)
+
+
+print(f'The numbers {number1} and {number2} '
+      f'add to {sum}')
+
+print(f'The total of all numbers multiplied together is {total}!')
+
+
+
