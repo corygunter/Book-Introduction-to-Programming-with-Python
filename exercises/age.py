e491ed388b6ee2bcfa2b9596a7973b014cb3e4f6
Cory Gunter <cory.j.gunter@gmail.com>
2024-06-07 13:39:38 -0400

    changed names of files

---
+++ exercises/age.py
@@ -0,0 +1,9 @@
+# Age
+age = int(input("How old are you?\n"))
+decade = 10
+
+print(f'You are {age} years old.')
+print(f'In {decade} years you will be {age + decade} years old.')
+print(f'In {decade * 2} years you will be {age + decade * 2} years old.')
+print(f'In {decade * 3} years you will be {age + decade * 3} years old.')
+print(f'In {decade * 4} years you will be {age + decade * 4} years old.')
